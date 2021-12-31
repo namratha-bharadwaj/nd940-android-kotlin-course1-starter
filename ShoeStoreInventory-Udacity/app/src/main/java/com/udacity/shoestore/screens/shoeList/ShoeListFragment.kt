bcd28d09934fc6fbc8b5c54ef87a51e62d85c202
Namratha Bharadwaj <nbharadwaj@paypal.com>
2021-12-31 11:04:41 -0800

    modify a few things

--- ShoeStoreInventory-Udacity/app/src/main/java/com/udacity/shoestore/screens/shoeList/ShoeListFragment.kt
+++ ShoeStoreInventory-Udacity/app/src/main/java/com/udacity/shoestore/screens/shoeList/ShoeListFragment.kt
@@ -1,10 +1,16 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.screens.shoeList
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.models.Shoe
 
 /**
  * A simple [Fragment] subclass.
@@ -13,4 +19,51 @@ import android.view.ViewGroup
  */
 class ShoeListFragment : Fragment() {
 
+    private lateinit var binding: FragmentShoeListBinding
+
+    private val shoeListViewModel: ShoeListViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+        binding.lifecycleOwner = this
+
+        shoeListViewModel.shoeList.observe(viewLifecycleOwner, {
+            if (it.isNotEmpty()) {
+                loadShoes(it)
+            }
+        })
+        binding.showListScreenFloatingActionBtn.setOnClickListener {
+            findNavController().navigate(ShoeListFragmentDirections.actionShoeListToAddShoe())
+        }
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    private fun loadShoes(shoeList: List<Shoe>?) {
+        context?.let {
+            val shoeContainer = binding.shoeListLayout
+            shoeList?.forEach { shoe ->
+                val shoeLayout = ShoeLayout(it)
+                shoeLayout.createShoe(shoe)
+                shoeContainer.addView(shoeLayout)
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }
